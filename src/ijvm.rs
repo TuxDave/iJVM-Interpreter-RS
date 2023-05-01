0efc3d841463257d3d436fbde80dbc0998df2872
TuxDave <godinodavide@gmail.com>
2023-05-01 20:12:13 +0200

    fetch started

--- src/ijvm.rs
+++ src/ijvm.rs
@@ -1,8 +1,24 @@
 mod method_area;
 
 pub use method_area::MethodArea;
+use crate::parser::Istruction;
 
 #[allow(non_camel_case_types)]
 pub struct IJVM {
-    pc: usize // program counter
+    pc: usize, // program counter
+    ir: Option<Istruction>, // istruction "register"
+    method_area: MethodArea,
+}
+impl IJVM {
+    fn fetch (&mut self) {
+        self.ir = if let Ok(istr) = self.method_area.fetch_absolute(self.pc) {
+            self.pc += 1;
+            //todo: fetch all the remaining fields, increment pc as needed and set the istruction
+            Some()
+        } else {
+            //todo: trigger error listener
+            println!("Error in fetch");
+            None
+        }
+    }
 }
