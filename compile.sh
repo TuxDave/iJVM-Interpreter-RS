83ffd843feff7a92ce83d0a83a165d43b20fd969
TuxDave <godinodavide@gmail.com>
2023-05-17 09:19:08 +0200

    migliorato compile.sh

--- compile.sh
+++ compile.sh
@@ -4,3 +4,9 @@ cargo build
 cbindgen --crate ijvm_cpp_wrapper --output ijvm_cpp_wrapper.h
 cp target/debug/*.a ../ijvm_inspector/
 mv *.h ../ijvm_inspector/
+cd ../ijvm_inspector/
+rm -rf build
+mkdir build
+cd build
+qmake ..
+make -j$(grep -c ^processor /proc/cpuinfo)
